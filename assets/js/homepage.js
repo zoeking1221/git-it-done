850b8adc391844f0a179ec45ca16aa15ba079183
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-11-19 20:33:11 -0500

    started getuserprepos function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUSerRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUSerRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUSerRepos();
+getUSerRepos("zoeking1221");
