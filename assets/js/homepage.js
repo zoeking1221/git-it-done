4bb7ec0309bf08bb85b9b8eca5b8071e9647b3cc
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-11-21 14:45:43 -0500

    added search by language feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 var getUSerRepos = function(user) {
@@ -43,8 +44,7 @@ var displayRepos = function(repos, searchTerm) {
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
     }
-    console.log(repos);
-    console.log(searchTerm);
+
 
     // clear old content
     repoContainerEl.textContent = "";
@@ -87,7 +87,31 @@ var displayRepos = function(repos, searchTerm) {
     }
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
 
 
